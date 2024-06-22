170f22b46369147f3f0149e43b7b3854c45588f9
Michael Chan <michael.w.chan01@gmail.com>
2024-06-21 23:46:37 -0400

    Test all imported functions

--- index.js
+++ index.js
@@ -12,9 +12,36 @@ console.log("Check whether 5 is prime: ");
 console.log("--------------------------");
 
 console.log(isPrime(5));
-//Print Prime numbers up to 23
+
+console.log("Print prime numbers up to 23:");
+console.log("--------------------------");
 primeNumbers(primeNumber);
+
+console.log("Print sum of numbers array: ");
+console.log("--------------------------");
 console.log(sum(numbers));
+
+console.log("Print average of numbers array: ");
+console.log("--------------------------");
+console.log(average(numbers));
+
+let foods = ['pizza', 'hot dog', 'burger', 'macaroni and cheese'];
+console.log("Print longest food in foods array:")
+console.log("--------------------------");
+console.log(longest_string(foods));
+console.log("Print foods longer than 5 in size:")
+console.log("--------------------------");
+console.log(stringsLongerThan(foods, 5));
+let n = 8;
+console.log("Print numbers from 1 to 8:")
+console.log("--------------------------");
+printNumbers(n);
+
+
+
 let unflattened_arr = [8, [2,3], [[2,5],6]];
 unflattened_arr = flattenArr(unflattened_arr);
+
+console.log("Print flattened array of numbers:");
+console.log("--------------------------");
 console.log(unflattened_arr);
